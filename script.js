3db6cdcd582bfb816e9af6f191b7735c8fff9718
kindast <35968192+Adam014@users.noreply.github.com>
2023-01-19 18:07:05 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+var loader = document.getElementById("preloader");
+
+
+window.addEventListener("load", function(){
+    loader.style.display = "none";
+})
+
+
+    function run(){
+        let htmlCode = document.getElementById("html-code").value;
+        let cssCode = document.getElementById("css-code").value;
+        let jsCode = document.getElementById("js-code").value;
+        let output = document.getElementById("output");
+
+        output.contentDocument.body.innerHTML = htmlCode + "<style>" + cssCode + "<style>";
+        output.contentWindow.eval(jsCode);
+    }
+
